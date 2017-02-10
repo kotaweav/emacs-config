f4507d439bb132f04e0832dfafe357a4614c2ef5
Kota Weaver <kweaver@bostonic.net>
2017-02-10 13:46:21 -0500

    backup files in different directory

--- init.el
+++ init.el
@@ -30,6 +30,15 @@
 
 (setq enable-local-eval 't)
 
+(setq
+   backup-by-copying t      ; don't clobber symlinks
+   backup-directory-alist
+    '(("." . "~/.saves"))    ; don't litter my fs tree
+   delete-old-versions t
+   kept-new-versions 6
+   kept-old-versions 2
+   version-control t) ; use versioned backups  
+
 (use-package evil
   :ensure t)
 (evil-mode 1)
