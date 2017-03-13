88e7957f93506c2405d86a0971b556a19757e85e
Kota Weaver <kweaver@bostonic.net>
2017-03-13 13:03:42 -0400

    add changes for usability as terminal emulator

--- init.el
+++ init.el
@@ -61,9 +61,15 @@
 (setq helm-split-window-in-side-p t)
 (global-set-key (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
+(global-set-key (kbd "C-x b") 'helm-buffers-list)
 
 (use-package multi-term
   :ensure t)
+(add-hook 'term-mode-hook (lambda()
+                            (yas-minor-mode -1)))
+(defun rename-term (name)
+  (interactive "s")
+  (rename-buffer (concat "*term* " name)))
 
 (use-package highlight-symbol
   :ensure t)
