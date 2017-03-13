f42975aa01786bfd8c676acaaba10299e6be50c0
Kota Weaver <kweaver@bostonic.net>
2017-03-13 13:06:09 -0400

    sudo through tramp

--- init.el
+++ init.el
@@ -39,6 +39,8 @@
    kept-old-versions 2
    version-control t) ; use versioned backups  
 
+(set-default 'tramp-default-proxies-alist (quote ((".*" "\\`root\\'" "/ssh:%h:"))))
+
 (use-package evil
   :ensure t)
 (evil-mode 1)
